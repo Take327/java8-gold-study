3ee838b3c49367edce97b7b2ef17861f10c373b4
Take327 <gatewayseed2@gmail.com>
2021-08-04 01:29:21 +0900

    lineNumberReader

--- src/chapter7/fileIO/Main.java
+++ src/chapter7/fileIO/Main.java
@@ -1,20 +1,38 @@
 package chapter7.fileIO;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.LineNumberReader;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
-		try (BufferedReader buf = new BufferedReader(new FileReader("public/sample.txt"))) {
+		try (LineNumberReader buf = new LineNumberReader(new FileReader("public/sample.txt"))) {
+			buf.setLineNumber(1);
 
+			System.out.println(buf.readLine());
 			System.out.println(buf.readLine());
 
 			buf.close();
-			throw new IOException();
+			//throw new IOException();
+			
+			String secondLine = Files.readAllLines(Paths.get("public/sample.txt")).get(0);
+			System.out.println(secondLine);
+			
+			
+			
+			PrintWriter w = new PrintWriter("public/write.txt");
+			w.print("あいうえお");
+			w.print("かきくけこ");
+			w.println("あいうえお");
+			w.println("かきくけこ");
+			
+			w.close();
 
 		} catch (IOException e) {
 			// TODO 自動生成された catch ブロック
