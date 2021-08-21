96a38b508acedcb3d770352c674a7fd890979703
Take327 <gatewayseed2@gmail.com>
2021-08-22 00:33:35 +0900

    mysql

--- src/chapter9/MysqlTest/Main.java
+++ src/chapter9/MysqlTest/Main.java
@@ -5,6 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
@@ -12,21 +13,24 @@ public class Main {
 		// TODO 自動生成されたメソッド・スタブ
 		// 変数の準備
 		Connection con = null;
+		//
+		Statement st = null;
 		PreparedStatement stmt = null;
 		ResultSet rs = null;
 
 		// SQL文の作成
-		String sql = "SELECT * FROM test";
+		String sql = "SELECT" + " * " + "FROM test";
 
 		try {
 			// JDBCドライバのロード
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			// Class.forName("com.mysql.cj.jdbc.Driver");
 			// データベース接続
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/test_db", "root", "root");
 			// SQL実行準備
-			stmt = con.prepareStatement(sql);
+			st = con.createStatement();
 			// 実行結果取得
-			rs = stmt.executeQuery();
+			rs = st.executeQuery(sql);
+			System.out.println(rs);
 
 			// データがなくなるまで(rs.next()がfalseになるまで)繰り返す
 			while (rs.next()) {
@@ -35,9 +39,9 @@ public class Main {
 
 				System.out.println(id + ":" + name);
 			}
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			System.out.println("JDBCドライバのロードでエラーが発生しました");
+			// } catch (ClassNotFoundException e) {
+			// e.printStackTrace();
+			// System.out.println("JDBCドライバのロードでエラーが発生しました");
 		} catch (SQLException e) {
 			System.out.println("データベースへのアクセスでエラーが発生しました。");
 			e.printStackTrace();
